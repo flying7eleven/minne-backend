e29818c2dc896c1ac68b7c5c785c3be971b2d1bb
Tim Janke <tim+github@janke.biz>
2023-01-20 16:34:21 +0100

    :recycle: Split the route.rs into different files

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,8 @@ extern crate diesel;
 
 pub mod fairings;
 pub mod models;
-pub mod routes;
+pub mod routes {
+    pub mod health;
+    pub mod version;
+}
 pub mod schema;
--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ fn setup_logging(logging_level: LevelFilter) {
 async fn main() {
     use log::{debug, error, info};
     use minne_backend::fairings::{BackendConfiguration, MinneDatabaseConnection, NoCacheFairing};
-    use minne_backend::routes::{check_backend_health, get_backend_version};
+    use minne_backend::routes::{health::check_backend_health, version::get_backend_version};
     use rocket::config::{Shutdown, Sig};
     use rocket::figment::{
         util::map,
