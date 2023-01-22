8dc4d84726261a8530c72d97c0f9a8fe7048ee0d
Tim Janke <tim+github@janke.biz>
2023-01-22 15:55:20 +0100

    :construction: Add a table which will hold the managed tasks

---
+++ migrations/2023-01-22-145017_create_task_list/up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tasks
+(
+    id         SERIAL PRIMARY KEY,
+    title      VARCHAR(255) NOT NULL,
+    owner      SERIAL       NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP    NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE tasks
+    ADD CONSTRAINT tasks_owner_fkey FOREIGN KEY (owner) REFERENCES users (id);
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,16 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    tasks (id) {
+        id -> Int4,
+        title -> Varchar,
+        owner -> Int4,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         first_name -> Varchar,
@@ -11,3 +21,7 @@ diesel::table! {
         updated_at -> Timestamp,
     }
 }
+
+diesel::joinable!(tasks -> users (owner));
+
+diesel::allow_tables_to_appear_in_same_query!(tasks, users,);
